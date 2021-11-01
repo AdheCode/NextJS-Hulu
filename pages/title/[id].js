77189b21b02974fb364097d4509dc5f70f170c64
AdheCode <adhep86@gmail.com>
2021-11-01 19:49:18 +0700

    first commit

---
+++ pages/title/[id].js
@@ -0,0 +1,34 @@
+import Head from 'next/head'
+import Header from '../../components/Header'
+import Hero from '../../components/Hero';
+import requests from '../../utils/requests'
+
+function title({results}) {
+  return (
+    <div>
+      <Head>
+        <title>Hulu 1.0</title>
+        <meta name="description" content="Hulu 1.0 by AdheCode" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <Hero results={results} />
+    </div>
+  );
+}
+
+export default title;
+
+
+export async function getServerSideProps(context){
+    const external_id = context.params.id;
+    const genre = 'findMovie';
+    const request = await fetch(`https://api.themoviedb.org/3/movie/${external_id}${requests[genre]?.url}`).then(res => res.json());
+  
+    return {
+      props : {
+        results: request
+      }
+    }
+  
+  }
